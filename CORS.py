4e89c25a32735e344e83ce6a18d384bd08f489a8
HarshPanchigar <harshpanchigar@gmail.com>
2026-07-01 22:44:22 +0530

    env-setup

--- CORS.py
+++ CORS.py
@@ -4,7 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origin = ["http://127.0.0.1:5501"]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins = origin,
@@ -18,4 +17,3 @@ def get_form():
     return{
         "message":"CORS ENABLE API"
     }
-
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+# import os
+# from dotenv import load_dotenv
+from config import settings
+
+
 
 app = FastAPI()
 
-origin = ["http://127.0.0.1:5501"]
+# load_dotenv()
+# SECRET_KEY = os.getenv("SECRET_KEY")
+# DB_URL = os.getenv("DB_URL")
+origins = settings.origins
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origin,
+    allow_origins = origins.split(",") if origins else [],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
